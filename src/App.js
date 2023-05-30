24d1b8e9d52a60592877faf3d0ae606bf919dcd0
manishsingh8 <manishsingh04031998@gmail.com>
2023-05-30 13:35:33 +0530

    App Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className="container">
+      <div className='search'>
+          <input type="search" 
+          placeholder='search..'
+          autoFocus
+          id="search"
+          className='searchTerm'
+          />
+      </div>
+     </div>
+    </>
   );
 }
 
 export default App;
+
+
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=e3cfc82aca73cd2d65ca02c82294a53f
+// https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=e3cfc82aca73cd2d65ca02c82294a53f
