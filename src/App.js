4a0c703b68169793b0592477d954d7424da64cca
manishsingh8 <manishsingh04031998@gmail.com>
2023-06-01 12:30:24 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import WeatherCard from "./WeatherCard";
 
 function App() {
   const [searchValue, setSearchValue] = useState("delhi");
-  const[info,setInfo] = useState({});
+  const [info, setInfo] = useState({});
 
+  // funtion to get the data from api
   const getWeatherInfo = async () => {
     try {
       let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=e3cfc82aca73cd2d65ca02c82294a53f`;
-
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
-
-      const { temp ,  pressure,humidity} = data.main;
-      const {speed} = data.wind;
-      console.log(temp);
-      console.log(speed);
+      const { temp, pressure, humidity } = data.main;
+      const { speed } = data.wind;
       const weathermood = data.weather[0].main;
       const city = data.name;
-      const { country ,sunset } = data.sys;
-
-      const myNewWeatherInfo ={
+      const { country, sunset } = data.sys;
+      const myNewWeatherInfo = {
         temp,
         pressure,
         humidity,
@@ -30,10 +25,9 @@ function App() {
         country,
         sunset,
         city,
-        weathermood
-      }
+        weathermood,
+      };
       setInfo(myNewWeatherInfo);
-      
     } catch (error) {
       console.log(error);
     }
@@ -42,11 +36,10 @@ function App() {
     getWeatherInfo();
   }, []);
 
-  
-
   return (
     <>
       {/* search bar */}
+
       <div className="main">
         <div className="inner">
           <div className="container">
@@ -67,25 +60,16 @@ function App() {
               </button>
             </div>
           </div>
-          <WeatherCard info={info}/>
+          <WeatherCard info={info} />
         </div>
-      </div> 
-      
+      </div>
     </>
   );
 }
 
 export default App;
 
-// apis
+// important links
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=e3cfc82aca73cd2d65ca02c82294a53f
 // https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=e3cfc82aca73cd2d65ca02c82294a53f
 // https://erikflowers.github.io/weather-icons/
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=e3cfc82aca73cd2d65ca02c82294a53f")
-//       .then(response=>{
-//           return response.JSON();
-//        })
-//        .then(data=>{
-//         console.log(data);
-//        })
-//     }
--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 const WeatherCard = ({ info }) => {
-
-     const[weatherState,setWeatherState] = useState("");
+  const [weatherState, setWeatherState] = useState("");
   const {
     temp,
     pressure,
@@ -12,85 +11,90 @@ const WeatherCard = ({ info }) => {
     city,
     weathermood,
   } = info;
-// converting the time
+  // converting the time
   let sec = sunset;
-  let date = new Date(sec * 1000);//time in milisecods
-  let time = `${date.getHours()}:${date.getMinutes()}`;   
+  let date = new Date(sec * 1000); //time in milisecods
+  let time = `${date.getHours()}:${date.getMinutes()}`;
 
-  useEffect(()=>{
-     if(weathermood){
-     switch(weathermood){
-        case "Clouds":setWeatherState("wi-cloudy");
-        break;
-        case "Haze":setWeatherState("wi-fog");
-        break;
-        case "Clear":setWeatherState("wi-day-sunny");
-        break;
-        case "Rain":setWeatherState("wi-showers");
-        break;
+  useEffect(() => {
+    if (weathermood) {
+      switch (weathermood) {
+        case "Clouds":
+          setWeatherState("wi-cloudy");
+          break;
+        case "Haze":
+          setWeatherState("wi-fog");
+          break;
+        case "Clear":
+          setWeatherState("wi-day-sunny");
+          break;
+        case "Rain":
+          setWeatherState("wi-showers");
+          break;
+          case "Mist":
+          setWeatherState("wi-dust");
+          break;
         default:
-             setWeatherState("wi-day-sunny");
-            break;
-        
-     }
-     }
-    
-  },[weathermood]);
- 
+          setWeatherState("wi-day-sunny");
+          break;
+      }
+    }
+  }, [weathermood]);
+
   return (
     <>
       {/* temp card */}
       <article className="widget">
-      <div className="weatherIcon">
-        <i className={`wi ${weatherState}`} style={{ fontSize: '120px',color:"skyblue" }}></i>
-      </div>
+        <div className="weatherIcon">
+          <i className={`wi ${weatherState}`} style={{ fontSize: "90px" }}></i>
+        </div>
 
         <div className="weatherInfo">
-        <div className="country">
-          <div className="temperature background">
-            <span>{temp}&deg;</span>
-          </div>
-          <div className="description background ">
-            <div className="weatherCondition">{weathermood}</div>
-            <div className="place">
-              {city} {country}
+          <div className="country">
+            <div className="temperature background">
+              <span>{temp}&deg;</span>
             </div>
-          </div>
-          <div className="date">{new Date().toLocaleString()}</div>
+            <div className="description background ">
+              <div className="weatherCondition">{weathermood}</div>
+              <div className="place">
+                {city} {country}
+              </div>
+            </div>
+            <div className="date">{new Date().toLocaleString()}</div>
           </div>
           {/* 4 devided section */}
           <div className="extra-temp">
             {/* <div className="temp-info-minmax"> */}
-              <div className="two-sided-section">
-                <p>
-                  <i className="wi wi-sunset"></i>
-                </p>
-                <p className="extra-info-leftside">
+            <div className="two-sided-section">
+              <p>
+                <i className="wi wi-sunset"></i>
+              </p>
+              <p className="extra-info-leftside">
                 {time}
-                  <br />
-                  Sunset
-                </p>
-              </div>
-              <div className="two-sided-section">
-                <p>
-                  <i className="wi wi-humidity"></i>
-                </p>
-                <p className="extra-info-leftside">
-                  {humidity}
-                  <br/>
-                  Humidity
-                </p>
-              </div>
+                <br />
+                Sunset
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className="wi wi-humidity"></i>
+              </p>
+              <p className="extra-info-leftside">
+                {humidity}
+                <br />
+                Humidity
+              </p>
+            </div>
             {/* </div> */}
-         
-          {/* <div className="weather-extra-info"> */}
+
+            {/* <div className="weather-extra-info"> */}
             <div className="two-sided-section">
               <p>
                 <i className="wi wi-rain"></i>
               </p>
               <p className="extra-info-leftside">
                 {pressure}
-                <br/>
+                <br />
                 Pressure
               </p>
             </div>
@@ -100,12 +104,12 @@ const WeatherCard = ({ info }) => {
               </p>
               <p className="extra-info-leftside">
                 {speed}
-                <br/>
+                <br />
                 Speed
               </p>
             </div>
           </div>
-          </div>
+        </div>
         {/* </div> */}
       </article>
     </>
